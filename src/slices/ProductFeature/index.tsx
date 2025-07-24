fe38cb183d1352b102aec345dacd8ae5aa8acdc8
Heze <64218603+Hcmwebs@users.noreply.github.com>
2025-07-24 15:48:04 +1200

    feat: add dynamic component imports and price formatting utility
    - Created a new index file for dynamic imports of components: Hero, ProductFeature, and ScrollText.
    - Implemented a price formatting utility to format prices in USD.
    - Added a TypeScript configuration file with strict settings and module resolution for Next.js.

---
+++ src/slices/ProductFeature/index.tsx
@@ -0,0 +1,87 @@
+import { FC } from "react";
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicText, SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText } from "@prismicio/react";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { createClient } from "@/prismicio";
+import { Bounded } from "@/components/Bounded";
+import { FadeIn } from "@/components/FadeIn";
+import { formatPrice } from "@/utils/formatters";
+
+/**
+ * Props for `ProductFeature`.
+ */
+export type ProductFeatureProps =
+  SliceComponentProps<Content.ProductFeatureSlice>;
+
+/**
+ * Component for "ProductFeature" Slices.
+ */
+const ProductFeature: FC<ProductFeatureProps> = async ({ slice }) => {
+  const client = createClient();
+  const fragrance = isFilled.contentRelationship(slice.primary.fragrance)
+    ? await client.getByID<Content.FragranceDocument>(
+        slice.primary.fragrance.id,
+      )
+    : null;
+
+  const formattedPrice = formatPrice(fragrance?.data.price);
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="overflow-hidden bg-black py-16 text-white md:py-24"
+    >
+      <div className="grid grid-cols-1 items-center gap-8 lg:grid-cols-3 lg:grid-rows-[auto,auto]">
+        <FadeIn
+          className="translate-y-16 opacity-0 lg:col-span-2 lg:row-span-2"
+          vars={{ duration: 1 }}
+        >
+          <PrismicNextImage
+            field={slice.primary.feature_image}
+            className="h-auto w-full object-cover"
+          />
+        </FadeIn>
+
+        <FadeIn className="lg:row-start -1 translate-y-16 space-y-6 self-start bg-white/10 p-10 opacity-0 lg:col-start-3">
+          <h2 className="text-3xl leading-tight font-semibold tracking-tight md:text-4xl">
+            <PrismicText field={slice.primary.heading} />
+          </h2>
+
+          <div className="max-w-lg text-base text-gray-300">
+            <PrismicRichText field={slice.primary.description} />
+          </div>
+        </FadeIn>
+
+        {/* fragrance */}
+
+        <FadeIn
+          className="animate-in relative translate-y-16 self-end bg-white/10 opacity-0 will-change-transform"
+          vars={{ duration: 1, delay: 1 }}
+        >
+          <PrismicNextImage
+            field={fragrance?.data.bottle_image}
+            className="mx-auto -mt-10 w-full -rotate-12 md:-mt-20"
+          />
+          <div className="flex justify-between p-10 pt-4">
+            <div className="space-y-1">
+              <h3>
+                <PrismicText
+                  field={fragrance?.data.title}
+                  fallback="Fragrance"
+                />
+              </h3>
+              <p className="mt-2 text-gray-400">Eau de Parfum</p>
+              <p>Button Link</p>
+            </div>
+            <p className="mt-4 text-gray-100" aria-label="Product price">
+              <span>{formattedPrice}</span>
+            </p>
+          </div>
+        </FadeIn>
+      </div>
+    </Bounded>
+  );
+};
+
+export default ProductFeature;
