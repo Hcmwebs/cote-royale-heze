6e5b4b8aa60a8f5accb0ca01840230c31ccf6a90
Heze <64218603+Hcmwebs@users.noreply.github.com>
2025-07-25 22:35:43 +1200

    added content to the FragranceList and started styling

--- src/slices/FragranceList/index.tsx
+++ src/slices/FragranceList/index.tsx
@@ -3,6 +3,7 @@ import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { Bounded } from "@/components/Bounded";
 import { PrismicNextLink } from "@prismicio/next";
+import { RevealText } from "@/components/RevealText";
 
 /**
  * Props for `FragranceList`.
@@ -18,13 +19,27 @@ const FragranceList: FC<FragranceListProps> = ({ slice }) => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="space-y-8 bg-black py-16 text-center text-white md:py-24"
     >
-      <p>{slice.primary.eyebrow}</p>
-      <PrismicRichText field={slice.primary.heading} />
-      <PrismicRichText field={slice.primary.body} />
-      {slice.primary.fragrances.map((item) => (
-        <PrismicNextLink field={item.fragrance}>Link</PrismicNextLink>
-      ))}
+      <div className="mx-auto space-y-8">
+        <p className="text-sm font-light tracking-[0.2em] uppercase">
+          {slice.primary.eyebrow}
+        </p>
+        <RevealText
+          field={slice.primary.heading}
+          as="h2"
+          id={`fragrance-list-heading-${slice.id}`}
+          align="center"
+          duration={1.5}
+          staggerAmount={0.3}
+          className="font-display text-5xl uppercase sm:text-6xl md:text-7xl lg:text-8xl"
+        />
+        <PrismicRichText field={slice.primary.heading} />
+        <PrismicRichText field={slice.primary.body} />
+        {slice.primary.fragrances.map((item) => (
+          <PrismicNextLink field={item.fragrance}>Link</PrismicNextLink>
+        ))}
+      </div>
     </Bounded>
   );
 };
