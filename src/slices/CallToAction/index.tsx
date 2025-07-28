25f15ff4a32cb3dd0a2313f2b34b03846ed67d12
Heze <64218603+Hcmwebs@users.noreply.github.com>
2025-07-28 19:30:12 +1200

    Refactor CallToAction slice to enhance structure and update mock data

--- src/slices/CallToAction/index.tsx
+++ src/slices/CallToAction/index.tsx
@@ -1,6 +1,10 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { FadeIn } from "@/components/FadeIn";
+import { RevealText } from "@/components/RevealText";
+import { ButtonLink } from "@/components/ButtonLink";
+import { Bounded } from "@/components/Bounded";
 
 /**
  * Props for `CallToAction`.
@@ -12,41 +16,22 @@ export type CallToActionProps = SliceComponentProps<Content.CallToActionSlice>;
  */
 const CallToAction: FC<CallToActionProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for call_to_action (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server@latest"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 🎙️ Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       * 📚 Documentation: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+      <FadeIn>{slice.primary.eyebrow}</FadeIn>
+      <RevealText id="cta-heading" field={slice.primary.heading} />
+
+      <FadeIn>
+        <PrismicRichText field={slice.primary.body} />
+      </FadeIn>
+      {slice.primary.button.map((link) => (
+        <FadeIn key={link.key}>
+          <ButtonLink field={link} variant={link.variant} />
+        </FadeIn>
+      ))}
+    </Bounded>
   );
 };
 
