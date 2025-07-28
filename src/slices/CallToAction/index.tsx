872585365149ef6f3a49774f822f12e90035ab48
Heze <64218603+Hcmwebs@users.noreply.github.com>
2025-07-29 10:34:11 +1200

    Styled and Refactor ed CallToAction component for improved structure and readability

--- src/slices/CallToAction/index.tsx
+++ src/slices/CallToAction/index.tsx
@@ -19,18 +19,40 @@ const CallToAction: FC<CallToActionProps> = ({ slice }) => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="relative overflow-hidden bg-[url('/background.avif')] bg-cover bg-center py-16 text-gray-50 md:py-28"
     >
-      <FadeIn>{slice.primary.eyebrow}</FadeIn>
-      <RevealText id="cta-heading" field={slice.primary.heading} />
-
-      <FadeIn>
-        <PrismicRichText field={slice.primary.body} />
-      </FadeIn>
-      {slice.primary.button.map((link) => (
-        <FadeIn key={link.key}>
-          <ButtonLink field={link} variant={link.variant} />
+      <div className="relative z-10 mx-auto max-w-4xl space-y-8 text-center">
+        <FadeIn
+          className="translate-y-2 text-sm font-light tracking-[.2em] uppercase"
+          vars={{ duration: 0.8 }}
+        >
+          {slice.primary.eyebrow}
         </FadeIn>
-      ))}
+        <RevealText
+          id="cta-heading"
+          field={slice.primary.heading}
+          as="h2"
+          className="font-display mx-auto max-w-3xl text-5xl sm:text-6xl md:text-7xl"
+          align="center"
+          staggerAmount={0.1}
+          duration={0.8}
+        />
+
+        <FadeIn
+          className="mx-auto max-w-2xl translate-y-2 text-lg text-balance text-gray-300"
+          vars={{ duration: 0.8, delay: 0.4 }}
+        >
+          <PrismicRichText field={slice.primary.body} />
+        </FadeIn>
+
+        <div className="mt-10">
+          {slice.primary.button.map((link) => (
+            <FadeIn key={link.key}>
+              <ButtonLink field={link} variant={link.variant} />
+            </FadeIn>
+          ))}
+        </div>
+      </div>
     </Bounded>
   );
 };
